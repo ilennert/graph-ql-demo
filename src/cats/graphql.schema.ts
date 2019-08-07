cc5ced8aff2454326141c69f279424e87eda9d21
Irv Lennert <irv.lennert@gmail.com>
2019-08-07 08:14:55 -0400

    Schema work

--- src/cats/graphql.schema.ts
+++ src/cats/graphql.schema.ts
@@ -5,11 +5,25 @@
  */
 
 /* tslint:disable */
+export class CreateAddressInput {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+}
+
 export class CreateCatInput {
     id?: string;
     name: string;
     age: number;
     breed: string;
+}
+
+export class UpdateAddressInput {
+    street?: string;
+    city?: string;
+    state?: string;
+    zip?: string;
 }
 
 export class UpdateCatInput {
@@ -18,11 +32,29 @@ export class UpdateCatInput {
     breed?: string;
 }
 
+export class Address {
+    id: string;
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+    stateProv: string;
+    zipPostal: string;
+}
+
 export class Cat {
     id: string;
     name: string;
     age: number;
     breed: string;
+}
+
+export class CatWithOwners {
+    id: string;
+    name: string;
+    age: number;
+    breed: string;
+    ownersRanges: Owner[];
 }
 
 export abstract class IMutation {
@@ -33,10 +65,32 @@ export abstract class IMutation {
     abstract updateCat(id: string, updateCatInput?: UpdateCatInput): Cat | Promise<Cat>;
 }
 
+export class Owner {
+    id: string;
+    name: string;
+    address: Address[];
+    birthdate: DateTime;
+}
+
+export class OwnerRange {
+    id: string;
+    owner: Owner;
+    start: DateTime;
+    end: DateTime;
+}
+
+export class PetStore {
+    id: string;
+    name: string;
+    address: Address[];
+}
+
 export abstract class IQuery {
     abstract cats(): Cat[] | Promise<Cat[]>;
 
     abstract cat(id: string): Cat | Promise<Cat>;
+
+    abstract catsWithOwners(): CatWithOwners[] | Promise<CatWithOwners[]>;
 }
 
 export abstract class ISubscription {
@@ -46,3 +100,5 @@ export abstract class ISubscription {
 
     abstract catUpdated(): Cat | Promise<Cat>;
 }
+
+export type DateTime = any;
